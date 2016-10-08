e6d5444f2d7ad793e7e934dab66999ac14aa9f16
bryanleitao <bryanleitao1991@gmail.com>
2016-10-08 12:56:33 -0300

    add enum tipo, modif personaje

--- src/main/java/modelo/Personaje.java
+++ src/main/java/modelo/Personaje.java
@@ -1,5 +1,23 @@
 package modelo;
 
 public class Personaje {
+	private String nombre;
+	private String tipo;
 
+	/* agregar hash de cualidades */
+	
+	private String getTipo() {
+		return tipo;
+	}
+	private void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	private String getNombre() {
+		return nombre;
+	}
+	private void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
 }
