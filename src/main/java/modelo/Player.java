d9f79407eafe97f78cb613d0090a8f7a315da9d2
zqlprovider <ezequiel.mdp@gmail.com>
2016-10-11 09:16:12 -0300

    Algunos cambios

--- src/main/java/modelo/Player.java
+++ src/main/java/modelo/Player.java
@@ -1,8 +1,23 @@
 package modelo;
 
+import java.util.ArrayList;
+
 public class Player {
 	private String nickname;
 	private int puntaje;
+	private ArrayList personajeSeleccionado;
 	
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public int getPuntaje() {
+		return puntaje;
+	}
+	public void setPuntaje(int puntaje) {
+		this.puntaje = puntaje;
+	}
 
 }
--- src/main/java/modelo/Tipo.java
+++ src/main/java/modelo/Tipo.java
@@ -1,5 +1,14 @@
 package modelo;
 
 public enum Tipo {
+	HEROE ("HEROE"),
+	VILLANO ("VILLANO");
+	
+	private String descripcion;
 
+	private Tipo(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	
 }
