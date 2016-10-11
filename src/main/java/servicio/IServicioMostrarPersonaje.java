7d44d61e9d92669a3d7f9b34ae90cf92a68889bd
bryanleitao <bryanleitao1991@gmail.com>
2016-10-11 16:27:31 -0300

    modifique la interfaz del servicio de mostrar el personaje y agregue los metodos en el service de M_P

--- src/main/java/servicio/IServicioMostrarPersonaje.java
+++ src/main/java/servicio/IServicioMostrarPersonaje.java
@@ -1,5 +1,11 @@
 package servicio;
+import modelo.Tipo;
 
 public interface IServicioMostrarPersonaje {
-
+	
+	public void Listar ();
+	
+	public void Listar (Tipo value);
+	
+	public void Crear ();
 }
--- src/main/java/servicio/ServicioMostrarPersonaje.java
+++ src/main/java/servicio/ServicioMostrarPersonaje.java
@@ -1,5 +1,22 @@
 package servicio;
+import modelo.Personaje;
+import modelo.Tipo;
 
-public class ServicioMostrarPersonaje {
+public class ServicioMostrarPersonaje implements IServicioMostrarPersonaje{
+
+	public void Listar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void Listar(Tipo value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void Crear() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
