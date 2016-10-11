932a4b6dd78732f8c9550babd2f518e3e884d81f
zqlprovider <ezequiel.mdp@gmail.com>
2016-10-11 09:28:16 -0300

    Despues hay que ver como implementarlos

--- src/main/java/servicio/IServicioDuelo.java
+++ src/main/java/servicio/IServicioDuelo.java
@@ -1,5 +1,11 @@
 package servicio;
 
 public interface IServicioDuelo {
+	
+	public void MostrarCualidad ();
+	
+	public void MostrarPuntaje (Player player1, Player player2);
+	
+	public void ComparaCualidad (Personaje personajeA, Personaje personajeB);
 
 }
