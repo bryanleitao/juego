620af0132bece08642646a9baeebce43eba06768
María Florencia González <florencia_.gonzalez@hotmail.com>
2016-10-15 11:56:45 -0300

    Servicio Usuario

--- src/main/java/servicio/ServicioUsuario.java
+++ src/main/java/servicio/ServicioUsuario.java
@@ -1,5 +1,26 @@
 package servicio;
 
-public class ServicioUsuario {
+import java.util.Scanner;
+
+import modelo.Personaje;
+import modelo.Player;
+import modelo.Tipo;
+import servicio.ServicioMostrarPersonaje;
+
+public class ServicioUsuario implements IServicioUsuario {
+
+	public void setNickname() {
+			
+	}
+	
+	public void elegirPersonaje() {
+				
+	}
+
+	public void modificarPuntaje() {
+		
+		
+		
+	}
 
 }
