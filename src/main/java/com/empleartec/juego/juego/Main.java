46eebfd6da6eb1f92c5c97e03cc61a689b94576f
bryanleitao <bryanleitao1991@gmail.com>
2016-10-15 11:39:19 -0300

    agregue lo de tipo en el main

--- src/main/java/com/empleartec/juego/juego/Main.java
+++ src/main/java/com/empleartec/juego/juego/Main.java
@@ -1,6 +1,7 @@
 package com.empleartec.juego.juego;
 
 import servicio.ServicioMostrarPersonaje;
+import modelo.Tipo;
 
 public class Main {
 	
@@ -9,10 +10,18 @@ public class Main {
 		
 		ServicioMostrarPersonaje SMP = new ServicioMostrarPersonaje(); 
 		
-		//descomentar para probar y mostrar todos los personajes
-		SMP.Listar();
+		Tipo [] tipos = Tipo.values();
 		
-		SMP.MostrarPersonaje(SMP.TraerPersonaje("Batman"));
+		for(Tipo t : tipos){
+			System.out.println(t);
+		}
+		
+		SMP.Listar(Tipo.SUPERHEROE);
+		
+		//descomentar para probar y mostrar todos los personajes
+//		SMP.Listar();
+//		
+//		SMP.MostrarPersonaje(SMP.TraerPersonaje("Batman"));
 		
 	}
 
