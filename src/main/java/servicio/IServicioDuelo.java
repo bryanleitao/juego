9947dcff1655791c20b6aa53ccdc72f02160b66c
zqlprovider <ezequiel.mdp@gmail.com>
2016-10-15 13:44:48 -0300

    La interface

--- src/main/java/servicio/IServicioDuelo.java
+++ src/main/java/servicio/IServicioDuelo.java
@@ -5,10 +5,10 @@ import modelo.Player;
 
 public interface IServicioDuelo {
 	
-	public void MostrarCualidad ();
+	public void MostrarCualidad (Personaje personaje);
 	
 	public void MostrarPuntaje (Player player1, Player player2);
-	
+	//Hay que ver como se implementa este metodo
 	public void ComparaCualidad (Personaje personajeA, Personaje personajeB);
 
 }
