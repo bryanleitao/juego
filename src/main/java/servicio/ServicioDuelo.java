8b6e09ed3c486ad1cf0ea5dbad10b8b89fd37a5e
zqlprovider <ezequiel.mdp@gmail.com>
2016-10-15 13:43:39 -0300

    Falta la comparacion de las cualidades

--- src/main/java/servicio/ServicioDuelo.java
+++ src/main/java/servicio/ServicioDuelo.java
@@ -1,23 +1,31 @@
 package servicio;
 
+import java.util.Map;
+import java.util.Map.Entry;
+
 import modelo.Personaje;
 import modelo.Player;
 
 public class ServicioDuelo implements IServicioDuelo{
 
-	public void MostrarCualidad() {
-		// TODO Auto-generated method stub
+	public void MostrarCualidad(Personaje personaje) {
+		
+		Map<String, Integer> map = personaje.getCualidades();
+		
+		for (Entry<String, Integer> entry : map.entrySet())
+		{
+		    System.out.println("Cualidad: " + entry.getKey() + " - Valor: " + entry.getValue());
+		}
 		
 	}
 
 	public void MostrarPuntaje(Player player1, Player player2) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Puntaje Jugador 1: "+ player1.getPuntaje() + "  //  Puntaje Jugador 2: "+ player2.getPuntaje());
 	}
 
 	public void ComparaCualidad(Personaje personajeA, Personaje personajeB) {
 		// TODO Auto-generated method stub
-		
+		//Hay que ver como se implementa este metodo
 	}
 
 }
