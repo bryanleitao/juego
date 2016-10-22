82f138095dd0a3f9ceae1917ebbbe4b59571cb14
María Florencia González <florencia_.gonzalez@hotmail.com>
2016-10-22 12:05:29 -0300

    menu

--- src/main/java/menu/Menu.java
+++ src/main/java/menu/Menu.java
@@ -1,5 +1,44 @@
 package menu;
 
-public class Menu {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+import dao.DaoCharacter;
+import modelo.Character;
+import modelo.Player;
+import modelo.Type;
+import servicio.ServicesPlayer;
+import servicio.ServicesCharacter;
+
+public class Menu {
+	
+ 	public void menuPlayer(Player p) {
+		
+			Scanner sc =new Scanner(System.in);		
+			System.out.print("Introduzca nickname del Player");       
+			p.setNickname(sc.nextLine());
+ 
+	}
+	
+	public void menuType(Player p) {
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Elija con que tipo de personajes jugara: (a)-SUPERHEROE o (b)-VILLANO");
+		String option = sc.nextLine();
+		switch (option){
+			case "a": p.setTypePlayer(Type.SUPERHEROE) ; break;
+			case "b": p.setTypePlayer(Type.VILLANO); break;
+			default: this.menuType(p); break;
+		}
+				
+		
+	}
+	
+	public void menuCharacters(Player p) {
+		
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Seleccione los personaje con el que desea jugar");
+			String character = sc.nextLine();
+			p.setCharacters(characters);
+	}
 }
---
+++ src/main/java/menu/MenuDuelo.java
@@ -0,0 +1,12 @@
+package menu;
+
+import modelo.Personaje;
+import dao.DaoPersonaje;
+
+//mostrar personaje i , elegir cualidad j, suma puntaje al jugador--- prioridad p1
+
+public class MenuDuelo {
+	
+	
+	
+}
