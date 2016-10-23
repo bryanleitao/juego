46d9e52b966719ba7258edc0f63ac1c1ed3d62da
María Florencia González <florencia_.gonzalez@hotmail.com>
2016-10-23 19:37:42 -0300

    Solucionado conflicto en IServicesDuel

--- src/main/java/servicio/IServicesDuel.java
+++ src/main/java/servicio/IServicesDuel.java
@@ -7,13 +7,9 @@ public interface IServicesDuel {
 	
 	//muestra las cualidades del pesonaje
 	public void showQualitie (Character personaje);
-<<<<<<< HEAD
-	//muestra el puntaje del jugador
-=======
-	
+	//
 	public void incrementScore (Player player);
-	
->>>>>>> db2dcaec60a1904cd83b671a42b48ea51aec3e74
+	//muestra el puntaje del jugador
 	public void showScore(Player player1, Player player2);
 	//Hay que ver como se implementa este metodo
 	public int compareQualitie(int qualitieValueA, int qualitieValueB);
