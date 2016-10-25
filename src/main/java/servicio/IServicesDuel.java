5a4555df346b505c2f6e830c650706fddd0e89cc
María Florencia González <florencia_.gonzalez@hotmail.com>
2016-10-25 12:17:34 -0300

    saque comentarios y modifique javadoc

--- src/main/java/servicio/IServicesDuel.java
+++ src/main/java/servicio/IServicesDuel.java
@@ -5,16 +5,13 @@ import modelo.Player;
 
 /**
  * Esta interfaz contiene los metodos abstractos que se redefiniran en el ServicioDuel.
- * @author Ezequiel & Florencia
+ * @author Ezequiel, Bryan & Florencia
  */
 
 public interface IServicesDuel {
 	
-	//muestra las cualidades del pesonaje
 	public void showQualitie (Character personaje);
-	//muestra el puntaje del jugador
 	public void showScore(Player player1, Player player2);
-	//Hay que ver como se implementa este metodo
 	public int compareQualitie(int qualitieValueA, int qualitieValueB);
 	public int compareScore(Player player1, Player player2);
 
