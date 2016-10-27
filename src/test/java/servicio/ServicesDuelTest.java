e6a6529f92b1f74c8b07e64f6ddaae1cd1a33ab8
María Florencia González <florencia_.gonzalez@hotmail.com>
2016-10-27 15:46:29 -0300

    termine test

--- src/test/java/servicio/ServicesDuelTest.java
+++ src/test/java/servicio/ServicesDuelTest.java
@@ -1,10 +1,11 @@
 package servicio;
 
 import static org.junit.Assert.*;
-import junit.framework.Assert;
 
 import org.junit.Test;
 import org.mockito.Spy;
+
+import modelo.Player;
 
 /**
  * Test del ServicesDuel.
@@ -16,44 +17,59 @@ public class ServicesDuelTest {
 	private ServicesDuel service = new ServicesDuel();
 	
 	/**
-	 * Test del metodo showQuality();
-	 */
-	@Test
-	public void givenACharacterWhenShowQalityThenPrint() {
-	    
-	}
-	
-	/**
-	 * Test del metodo showScore();	
-	 */
-	@Test
-	public void givenAPlayerwhenShowScoreThenPrint() {
-	    
-	}
-
-	/**
 	 * Test del metodo compareQuality();
 	 */
 	@Test
-	public int nfaljn() {
-		return 0;
+	public void compareQualityTestCaseA() {
+		int result = service.compareQuality(250, 250);
 		
+		assertEquals(0, result);			
+	}
+	@Test
+	public void compareQualityTestCaseB() {
+		int result = service.compareQuality(200, 150);
+		
+		assertEquals(1, result);		
+	}
+	@Test
+	public void compareQualityTestCaseC() {
+		int result = service.compareQuality(20, 80);
+		
+		assertEquals(2, result);		
 	}
 	
 	/**
 	 * Test del metodo compareScore();
 	 */
+	
 	@Test
-	public void givenWhenThen() {
-		//given
+	public void compareScoreTestCaseA() {
+		Player player1 = new Player();
+		Player player2 = new Player();
+		player1.setScore(0);
+		player2.setScore(0);
+		int result = service.compareScore(player1, player2);
 		
+		assertEquals(0, result);		
+	}	
+	@Test
+	public void compareScoreTestCaseB() {
+		Player player1 = new Player();
+		Player player2 = new Player();
+		player1.setScore(2);
+		player2.setScore(1);
+		int result = service.compareScore(player1, player2);
 		
-		//when
+		assertEquals(1, result);		
+	}	
+	@Test
+	public void compareScoreTestCaseC() {
+		Player player1 = new Player();
+		Player player2 = new Player();
+		player1.setScore(1);
+		player2.setScore(2);
+		int result = service.compareScore(player1, player2);
 		
-		
-		//then
-		
+		assertEquals(2, result);		
 	}
 }
-
-
